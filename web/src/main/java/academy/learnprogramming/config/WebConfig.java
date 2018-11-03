873fe7c8d687b221cf2d65c718f591ff1cc5b026
Wojtek Dudek <wdudek82@gmail.com>
2018-11-03 15:47:38 +0100

    Added locale change interceptor with lang parameter allowing to dynamically change language version

--- web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -2,13 +2,22 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.interceptor.RequestInterceptor;
 import academy.learnprogramming.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -18,5 +27,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestInterceptor());
+
+        LocaleChangeInterceptor localeChangeInterceptor =
+                new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+
+        registry.addInterceptor(new LocaleChangeInterceptor());
     }
 }
